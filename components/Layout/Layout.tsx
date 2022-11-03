0b18c979eff7b096b62f44ebd87d7d8af7647674
laribright <68712283+laribright@users.noreply.github.com>
2022-11-03 08:07:52 +0100

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: FC<LayoutProps> = (props) => {
 
   return (
     <div className="md:p-5 lg:p-[50px] w-screen h-screen">
-      <span className="absolute top-[15px] opacity-40 hidden md:block left-1/2 -translate-x-1/2 font-semibold lowercase">TALENT AFRICA TYPING CHALLENGE TECHNICAL TEST</span>
+      <span className="absolute top-[15px] opacity-40 hidden md:block left-1/2 -translate-x-1/2 font-semibold lowercase">Typing Speed Game</span>
       <main className="bg-gradient-to-l grid from-[#3E0040] to-[rgba(62,0,64,0.54)] w-full h-full p-3 md:p-[30px] lg:p-[64px] md:rounded-[50px]">
         {children}
       </main>
